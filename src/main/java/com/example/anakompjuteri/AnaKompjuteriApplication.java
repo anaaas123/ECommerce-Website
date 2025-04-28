861e36f03d32f47b130b4d21c5a6d4133ce68c19
ana.servinovska <ana.servinovska@itgma.com>
2025-04-28 11:26:40 +0200

    Initial commit

---
+++ src/main/java/com/example/anakompjuteri/AnaKompjuteriApplication.java
@@ -0,0 +1,13 @@
+package com.example.anakompjuteri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnaKompjuteriApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnaKompjuteriApplication.class, args);
+    }
+
+}
